02343a502acde94ebe546f14fabb5a8d862b917b
Sai_Kaizu <107693463+saipratama@users.noreply.github.com>
2022-06-27 21:01:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tiitle>Pinang Aquarium | Login</tittle>
+</head>
+<body>
+    <h1>Pinang Aquarium | Login</h1>
+    <h3>Halaman Login</h3>
+    <form action="login.php" mwthod="post">
+        <table>
+            <tr>
+                <td>username</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="login" value="Log In"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
